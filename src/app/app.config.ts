9993c1e61fb96530ef3006cf4265cc8417387835
Matthieu <matthieu.delarue5@gmail.com>
2025-02-02 16:05:29 +0100

    ✨ Add PostCSS configuration and update styles for improved icon support and SEO

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from "@angular/router";
 
 import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
 import { routes } from "./app.routes";
+import { provideNgIconsConfig } from "@ng-icons/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration(withEventReplay())],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    // Define the default configuration for icons
+    provideNgIconsConfig({
+      size: "1.5em",
+      strokeWidth: 2,
+      color: "#030712",
+    }),
+  ],
 };
